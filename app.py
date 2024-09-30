bbb23fbed18901638db284c34a2d7bc3e300ebca
sarangs-ntnu <117364041+sarangs-ntnu@users.noreply.github.com>
2024-09-30 17:37:38 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -33,6 +33,42 @@ def predict_course_aspect(feedback):
 # Streamlit app layout
 st.set_page_config(page_title="Aspect-based Sentiment Anlaysis of Student Feedback", layout="centered", initial_sidebar_state="auto")
 
+st.markdown("""
+#### This application analyzes the student feedback to determine whether it is about a teacher or a course, detects sentiment, and identifies important teacher or course aspects.
+""")
+
+# Get user input
+user_input = st.text_area("Enter the feedback or comments for analysis:", height=200)
+
+if st.button("Analyze Text"):
+    if user_input.strip():
+        # Predict whether it's about teacher or course
+        type_result = predict_teacher_course(user_input)
+        sentiment_result = predict_sentiment(user_input)
+        if type_result == 'teacher':
+            aspect_result = predict_teacher_aspect(user_input)
+        else:
+            aspect_result = predict_course_aspect(user_input)
+
+        # Display the results in a nice way
+        st.subheader("Analysis Results")
+
+        st.markdown(f"**Type:** `{type_result}`")
+        
+        st.markdown(f"**Sentiment:** `{sentiment_result}`")
+        
+        st.markdown("**Aspect Identified:**")
+        st.write(", ".join(aspect_result))
+    else:
+        st.error("Please enter some text for analysis.")
+
+# Add a footer
+st.markdown("---")
+st.markdown("**Developed by Sarang Shaikh**")
+st.markdown("""
+Feel free to reach out for more information or suggestions!
+""")
+
 
 
 
